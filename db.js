8107762a35bc4a874013382011a19297f53d16ee
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2024-06-27 08:25:08 +0700

    be

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2"
 export const db = mysql.createConnection({
-    host:"127.0.0.1",
-    user:"root",
-    password:"962002",
-    database:"elearning"
+    host:"bvvkoy5zhzrua05wkttv-mysql.services.clever-cloud.com",
+    user:"ut6htjauqzone7em",
+    password:"FpJ0bMOxPyCOYP9HrhK1",
+    database:"bvvkoy5zhzrua05wkttv"
 })
